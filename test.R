b2adca426b655da24beaa57ebb6dafb0836eb851
Paul CARTERON <68987339+paul-carteron@users.noreply.github.com>
2022-10-30 15:51:59 +0100

    Create test.R

---
+++ test.R
@@ -0,0 +1,14 @@
+library(sf)
+
+dTolerance <- 0
+
+x <- st_sfc(st_point(c(-0.49, 45.42)), crs = 4326)
+x <- st_as_sfc(x)
+
+res <- x %>%
+      st_make_valid() %>%
+      st_union() %>%
+      st_transform(4326) %>%
+      st_simplify(dTolerance = dTolerance)
+      
+print(res)
