bba818b4825dcd92caf091096030efff5813b95a
Paul CARTERON <68987339+paul-carteron@users.noreply.github.com>
2022-10-31 10:11:45 +0100

    Update test2.R

--- test2.R
+++ test2.R
@@ -1,14 +1,9 @@
 library(sf)
 library(httr2)
+library(tibble)
 
 # Test read_sf from url
 url <- "https://apicarto.ign.fr/api/gpu/zone-urba?geom=%7B%22type%22%3A%22Point%22%2C%22coordinates%22%3A%5B-0.49000000000000007%2C45.419999999999998%5D%7D"
-res1 <- read_sf(url)
-print(res1)
+resp <- read_sf(url)
+print(resp)
 
-# Test read_sf from httr2 string response
-# res2 <- request(url) %>%
-#   req_perform() %>%
-#   resp_body_string() %>% 
-#   read_sf()
-# print(res2)
