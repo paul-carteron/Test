3fb048b066a82d1af082120b613b17135d2c41bf
Paul CARTERON <68987339+paul-carteron@users.noreply.github.com>
2022-10-31 09:02:11 +0100

    Update test.R

--- test.R
+++ test.R
@@ -1,13 +1,30 @@
 library(sf)
+library(geojsonsf)
+library(methods)
 
-dTolerance <- 0
-
+# input
 x <- st_sfc(st_point(c(-0.49, 45.42)), crs = 4326)
+ressource = "zone-urba"
+partition = NULL
+categorie = NULL
+dTolerance = 0
 
-res <- x %>%
+# Function
+if (!is.null(partition)){
+      x <- NULL
+}
+
+if (methods::is(x, "sf")){
+      x <- st_as_sfc(x)
+}
+
+# prepare shape
+shape <- x %>%
       st_make_valid() %>%
       st_union() %>%
       st_transform(4326) %>%
       st_simplify(dTolerance = dTolerance)
-      
+
+# convert as geojson
+res <- sfc_geojson(shape)
 print(res)
