dec5aa6a1e19b4cb0a1a7e8d585f781399f841cd
Paul CARTERON <68987339+paul-carteron@users.noreply.github.com>
2022-10-31 10:36:58 +0100

    Update test.R

--- test.R
+++ test.R
@@ -1,4 +1,5 @@
 library(sf)
+library(tibble)
 
 # Test read_sf from url
 url <- "https://apicarto.ign.fr/api/gpu/zone-urba?geom=%7B%22type%22%3A%22Point%22%2C%22coordinates%22%3A%5B-0.49000000000000007%2C45.419999999999998%5D%7D"
