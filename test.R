5abc08162d23132bcf351d1ca38746f18038a0d4
Paul CARTERON <68987339+paul-carteron@users.noreply.github.com>
2022-10-31 09:28:20 +0100

    Update test.R

--- test.R
+++ test.R
@@ -1,6 +1,7 @@
 library(sf)
 library(geojsonsf)
 library(methods)
+library(httr2)
 
 # input
 x <- st_sfc(st_point(c(-0.49, 45.42)), crs = 4326)
@@ -25,6 +26,23 @@ shape <- x %>%
       st_transform(4326) %>%
       st_simplify(dTolerance = dTolerance)
 
-# convert as geojson
-res <- sfc_geojson(shape)
+# param
+param <- list(
+      geom = switch(is.null(x) + 1, sfc_geojson(shape), NULL),
+      partition = partition,
+      categorie = categorie
+)
+
+# build url
+req <- request("https://apicarto.ign.fr/api/gpu") %>%
+      req_user_agent("happign (https://paul-carteron.github.io/happign/)") %>%
+      req_url_path_append(ressource) %>%
+      req_url_query(!!!param)
+print(req)
+
+# made request
+res <- req %>%
+      req_perform() %>%
+      resp_body_string() %>%
+      read_sf()
 print(res)
