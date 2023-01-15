a231e69b2b774b580832738a48968be69e26781c
Paul CARTERON <68987339+paul-carteron@users.noreply.github.com>
2023-01-15 14:44:38 +0100

    Update test.R

--- test.R
+++ test.R
@@ -1,7 +1,9 @@
 library(sf)
 
-# Test read_sf from url
-url <- "https://apicarto.ign.fr/api/gpu/zone-urba?geom=%7B%22type%22%3A%22Point%22%2C%22coordinates%22%3A%5B-0.49000000000000007%2C45.419999999999998%5D%7D"
-resp <- read_sf(url)
-print(resp)
+pt <- st_sfc(st_point(c(0, 60)), crs = 2154)
+st_axis_order(F)
+print(st_transform(pt, 4326)[[1]])
+
+st_axis_order(T)
+print(st_transform(pt, 4326)[[1]])
 
