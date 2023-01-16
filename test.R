149f40c5ca49ccc174f19f06cd1de3300cdcf47b
Paul CARTERON <68987339+paul-carteron@users.noreply.github.com>
2023-01-16 20:04:58 +0100

    Update test.R

--- test.R
+++ test.R
@@ -1,10 +1,10 @@
 library(sf)
-
-shape <- st_sfc(st_point(x = c(-4.344, 47.813)), crs = 4326)
 crs <- 4326
-print(st_axis_order())
 
 # case 1 : crs == 4326 & st_crs(shape)$epsg == 4326
+shape <- st_sfc(st_point(x = c(-4.344, 47.813)), crs = 4326)
+print( st_as_text(st_geometry(shape)))
+print(st_axis_order())
 st_crs(shape)$epsg # expect : 4326
 st_axis_order(T)
 print(st_axis_order())
@@ -13,9 +13,11 @@ print( st_as_text(st_geometry(shape))) #expect : "POINT (47.813 -4.344)"
 st_axis_order(F)
 print(st_axis_order())
 
-shape <- st_sfc(st_point(x = c(151147, 6771387)), crs = 2154)
-st_crs(shape)$epsg # expect : 2154
 # case 2 : crs == 4326 & st_crs(shape)$epsg != 4326
+shape <- st_sfc(st_point(x = c(151147, 6771387)), crs = 2154)
+print( st_as_text(st_geometry(shape)))
+print(st_axis_order())
+st_crs(shape)$epsg # expect : 2154
 st_axis_order(T)
 print(st_axis_order())
 shape <- st_transform(shape, 4326) 
